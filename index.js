827fbffc5841276ce4c19ab5f50754f09e1f5d4a
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-14 13:25:04 +0600

    done react query and update all  services

--- index.js
+++ index.js
@@ -34,18 +34,20 @@ async function run() {
       res.send(services);
     });
 
+    // This is not the proper way to query.
+    //After learning more about mongodb . use aggregate lookup , pipeline
     app.get("/available", async (req, res) => {
-      const date = req.query.date || "May 11, 2022";
+      const date = req.query.date;
 
       //step 1: get all services
       const services = await servicesCollection.find().toArray();
 
-      //setp 2: get the booking of that day
+      //setp 2: get the booking of that day . output : [{}, {}, {}, {},{}, {}, {}, {}]
       const query = { date: date };
       const bookings = await bookingCollection.find(query).toArray();
 
       //step 3: for each service , find bookings for that service
-      services.forEach((service) => {
+      /* services.forEach((service) => {
         const serviceBooking = bookings.filter(
           (booking) => booking.treatment === service.name
         );
@@ -53,7 +55,21 @@ async function run() {
         const available = service.slots.filter(s =>!booked.includes(s))
         service.available =available
        
-      });
+      }); */ 
+
+      // step 3
+      services.forEach(service => {
+        //step 4: find booking for that service . output : [{}, {}, {}, {}]
+        const serviceBookings= bookings.filter(book => book.treatment === service.name)
+        // step 5: select slots for the service bookings: ["", "", "", ""]
+        const bookedSlots = serviceBookings.map(book=> book.slot)
+        // step 6: select those slotes that are not in bookedSlots
+        const available = service.slots.filter(slot => !bookedSlots.includes(slot))
+        //step 7:set available to slots to make it esier
+
+
+        service.slots = available
+      })
 
       res.send(services);
     });
